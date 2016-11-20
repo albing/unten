41352d3f9d4e58c65e61395d9625270c2aa63a0f
Greg Albing <gregalbing@gmail.com>
2016-11-20 00:02:52 -0600

    Add descriptive comments

--- unter.py
+++ unter.py
@@ -5,36 +5,49 @@ app = Flask(__name__)
 cars = [1,2,3,4]
 
 #
-# DETAILED DESCRIPTION OF API CALL GOES HERE
+# /location/
+#
+# Things to do with locations
+#
+# As a driver, I want to:
+# 	* update my location so that my passengers can see how close I am to picking them up
+#
+# As a passenger, I want to:
+# 	* alert a driver where I want to be picked up
 #
 @app.route("/location")
 @app.route("/location/")
 @app.route("/location/<int:car_number>", methods=['GET', 'PUT'])
 def location(car_number=None):
     if request.method == "PUT":
-    	return location_post(car_number)
+    	return location_put(car_number)
     	return "posted"
     elif request.method == "GET":
 	return location_request(car_number)
     else:
 	return "PANIC"
 
-# description of child function goes here
-def location_post(car_number):
+# PUT method
+# changes the location of a driver or requester
+def location_put(car_number):
     if car_number in cars:
 	return "Already have one"
     else:
 	cars.append(car_number)
 	return "Okay"
 
-# description of child function goes here
+
+# GET method
+# Get the location of a person.
+# If no person is given, get a listing of all possible people
 def location_request(car_number):
     if car_number == None:
-	ret_val = "list of cars:<br><ul>"
-	for car in cars:
-	    ret_val += "<li>" + "<a href='" + url_for('location', car_number=car) + "'>" + str(car) + "</a></li>"
-	ret_val += "</ul>"
+	# json: TODO: use 3rd-party formatter
+	ret_val = "{cars:["
+	ret_val += ",".join([str(x) for x in cars])
+	ret_val += "]}"
 	return ret_val
+	# TODO: allow for other formats
     else:
 	return "Location of car number: " + str(car_number)
 
